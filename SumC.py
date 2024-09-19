258b135d93ec51e4629137fb0c755ae2c202bf62
Arashi <2814835988@qq.com>
2024-09-19 22:28:33 +0800

    新建两个数的和python

---
+++ SumC.py
@@ -0,0 +1,10 @@
+# 定义一个函数来计算两个数的和
+def add_numbers(a, b):
+    return a + b
+
+# 使用函数并打印结果
+num1 = 5
+num2 = 10
+result = add_numbers(num1, num2)
+
+print(f"The sum of {num1} and {num2} is {result}")
